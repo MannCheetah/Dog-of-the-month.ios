3a075ea591c48ad4b8965e2f5c37f4f3aaaab9a5
Michael Cuellar <Michaeldev720@icloud.com>
2023-03-12 23:56:20 -0700

    Dog--of-the-month

--- Dog-of-the-month/ContentView.swift
+++ Dog-of-the-month/ContentView.swift
@@ -9,13 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        VStack(alignment: .leading, spacing: 0.0) {
+            Image("Leo")
+                .resizable(resizingMode: .stretch)
+                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                .cornerRadius(70)
+            HStack(){
+                Text("Name:Leo")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Spacer()
+                    
+                    
+                VStack{
+                    
+                    HStack{
+                        Image(systemName:"star.fill")
+                        Image(systemName:"star.fill")
+                        Image(systemName:"star.fill")
+                        Image(systemName:"star.fill")
+                        Image(systemName:"star.leadinghalf.filled")
+                        
+                    }
+                    .foregroundColor(.yellow)
+                    .font(.caption)
+                    
+ 
+                    Text("(Reviews: 420)")
+                        
+                }
+                
+            }
+
+                
+            Text("Breed:Labrador Retriever")
+                .fontWeight(.bold)
+            Text("Age:2")
+                .fontWeight(.bold)
+            Text("Weight:65lbs")
+                .fontWeight(.bold)
+            Text("Personality:Spoiled")
+                .fontWeight(.bold)
         }
         .padding()
+        .background(Rectangle()
+            .foregroundColor(.white)
+            .cornerRadius(15)
+            .shadow(radius: 15))
+        .padding()
+        
     }
 }
 
