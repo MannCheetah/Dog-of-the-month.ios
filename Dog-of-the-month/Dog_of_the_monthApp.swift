7c7e6a020dccf05395253c78693aca2b58d246d3
Michael Cuellar <Michaeldev720@icloud.com>
2023-03-12 20:05:25 -0700

    Initial Commit

---
+++ Dog-of-the-month/Dog_of_the_monthApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dog_of_the_monthApp.swift
+//  Dog-of-the-month
+//
+//  Created by Michael Cuellar on 3/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Dog_of_the_monthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
